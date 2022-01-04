f7841d148f6801052674a8068273040fd15cdc3b
David Dreher <david@md-web-solutions.de>
2022-01-04 21:43:49 +0100

    feature(quirion): Add also support to parse sell acivities (#4854)
    * fix(quirion): Resolve build errors and simplified some stuff
    
    * Fix type `Buy` detection
    
    * Add additional account statement #245
    
    * Adjust implementation to parse also sell activities
    
    * update docs
    
    * prettier
    
    Co-authored-by: David Dreher <dredav@users.noreply.github.com>

--- tests/brokers/quirion.test.js
+++ tests/brokers/quirion.test.js
@@ -1,13 +1,51 @@
 import * as quirion from '../../src/brokers/quirion';
 import { validateAllSamples } from '../setup/brokers';
-import { allSamples, buySamples, dividendSamples } from './__mocks__/quirion';
+import {
+  allSamples,
+  statementsSamples,
+  dividendSamples,
+} from './__mocks__/quirion';
 
 describe('Broker: quirion', () => {
   validateAllSamples(quirion, allSamples);
 
-  describe('Validate buys', () => {
-    test('Can parse a Kontoauszug', () => {
-      const activities = quirion.parsePages(buySamples[0]).activities;
+  describe('Validate account statements', () => {
+    test('Can parse document: 20201102', () => {
+      const activities = quirion.parsePages(statementsSamples[1]).activities;
+
+      expect(activities.length).toEqual(32);
+
+      expect(activities[0]).toEqual({
+        broker: 'quirion',
+        type: 'Sell',
+        date: '2020-10-05',
+        datetime: '2020-10-05T' + activities[0].datetime.substring(11),
+        isin: 'IE00B42THM37',
+        company: 'Dimensional Fds-Emerg.MktsVa. Registered Shares EUR Dis.o.N.',
+        shares: 9.977,
+        price: 8.209882730279643,
+        amount: 81.91,
+        fee: 0,
+        tax: 0,
+      });
+
+      expect(activities[31]).toEqual({
+        broker: 'quirion',
+        type: 'Buy',
+        date: '2020-10-28',
+        datetime: '2020-10-28T' + activities[31].datetime.substring(11),
+        isin: 'LU1931974692',
+        company: 'Amundi Index Solu.-A.PRIME GL.Nam.-Ant.UCI.ETF DR USD Dis.oN',
+        shares: 0.184,
+        price: 20,
+        amount: 3.68,
+        fee: 0,
+        tax: 0,
+      });
+    });
+
+    test('Can parse document: 20210731', () => {
+      const activities = quirion.parsePages(statementsSamples[0]).activities;
 
       expect(activities.length).toEqual(24);
 
