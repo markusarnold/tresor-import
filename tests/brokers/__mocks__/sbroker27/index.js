b050cb74e20e6b01e04005238548ea5114d7cfe4
David Dreher <david@md-web-solutions.de>
2022-01-08 11:58:47 +0100

    feature(sBroker): Add support for the new sbroker documents (#4883)
    * Add sample from #4701
    
    * Add parser for sbroker v2.7
    
    * Add sample from #4761
    
    * Adjust parser for sample from #4761
    
    * Add sample from #4631
    
    * Adjust parser for sample from #4631
    
    * Add sample from #4880
    
    * Adjust parser for sample from #4880
    
    * Add samples from #4719
    
    * Adjust parser for sample from #4719
    
    * fix declaration
    
    * review stuff
    
    Co-authored-by: David Dreher <dredav@users.noreply.github.com>

---
+++ tests/brokers/__mocks__/sbroker27/index.js
@@ -0,0 +1,13 @@
+export const buySamples = [
+  require('./buy/2021_GB0002875804.json'),
+  require('./buy/2021_LU0322253906.json'),
+  require('./buy/2021_IE00BFY0GT14.json'),
+];
+
+export const dividendSamples = [
+  require('./dividend/2021_US88579Y1010.json'),
+  require('./dividend/2021_NO0003054108.json'),
+  require('./dividend/2021_US1713401024.json'),
+];
+
+export const allSamples = buySamples.concat(dividendSamples);
